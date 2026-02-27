3a30a6c90e3064b04337c57099c9b8992b3be1d4
akocar <kocaralinihat@gmail.com>
2026-02-27 12:27:13 -0500

    .gitignore changes

--- backend/src/main/java/me/haznedar/wallet/transaction/entity/Transaction.java
+++
@@ -1,9 +0,0 @@
-package me.haznedar.wallet.transaction.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "transactions")
-public class Transaction {
-}
